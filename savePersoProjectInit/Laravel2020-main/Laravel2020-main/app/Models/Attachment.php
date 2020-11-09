18f6834a4612cb88c5953c93be7a9a5056106744
carolusDev <clement.charlemagne@ynov.com>
2020-11-09 08:10:05 +0100

    CHARLEMAGNE, initial commit, migrations added

---
+++ savePersoProjectInit/Laravel2020-main/Laravel2020-main/app/Models/Attachment.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attachment extends Model
+{
+    use HasFactory;
+    public function create()
+    {
+        return $this->hasOne('App\Models\User', 'foreign_key');
+    }
+    public function belongTo()
+    {
+        return $this->hasOne('App\Models\Task', 'foreign_key');
+    }
+}
